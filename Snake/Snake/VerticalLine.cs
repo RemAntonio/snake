9853e4f3fecd1908e1f0726771de2c532c43639a
RemAntonio <ave_darknes@mail.ru>
2016-06-19 12:44:56 +0300

    Создание горизонтальных линий из символов и вывод их на консоль. Так же пробовалось создание вертикальных линий с последующим выводом в на консоль. Эксперимент прошел успешно =)

---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+    {
+    class VerticalLine
+        {
+        List<Point> pList2;
+
+        public VerticalLine ( int yTop, int yDown, int x, char sym )
+            {
+            pList2 = new List<Point>();
+            for ( int y = yTop ; y <= yDown ; y++ )
+                {
+                Point p2 = new Point( x, y, sym );
+                pList2.Add( p2 );
+                }
+
+            }
+        public void Drow ( )
+            {
+            foreach ( Point p2 in pList2 )
+                {
+                p2.Draw();
+                }
+            }
+        }
+    }
